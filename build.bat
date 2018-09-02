a2aaa9c8b12e0e67d7dc16eae047bdf3455bd226
Ted John <ted@brambles.org>
2018-09-03 00:06:59 +0100

    Setup for nuget publishing

--- build.bat
+++ build.bat
@@ -2,11 +2,11 @@ pushd vcpkg
 
 REM Install libraries
 .\vcpkg install sdl2:%TRIPLET% ^
-	yaml-cpp:%TRIPLET%
+    yaml-cpp:%TRIPLET%
 
 REM Export libraries
 .\vcpkg export sdl2:%TRIPLET% ^
-	yaml-cpp:%TRIPLET% ^
-	--zip --nuget
+    yaml-cpp:%TRIPLET% ^
+    --zip --nuget "--nuget-id=OpenLoco.Dependencies" "--nuget-version=%VERSION%"
 
 popd
--- package.bat
+++
@@ -1,9 +0,0 @@
-set packages=vcpkg\installed\%TRIPLET%
-set outarchive=openloco-libs-v%version%-%TRIPLET%
-
-pushd %packages%
-
-REM create zip
-7z a -tzip -mx9 -mtc=off "%outarchive%.zip" "*"
-
-popd
