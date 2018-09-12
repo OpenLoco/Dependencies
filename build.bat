5037b8698d7309827b92834a4fe337a5c3be1085
Ted John <ted@brambles.org>
2018-09-12 12:27:13 +0100

    Add SDL2 mixer

--- build.bat
+++ build.bat
@@ -1,11 +1,15 @@
 pushd vcpkg
 
 REM Install libraries
-.\vcpkg install sdl2:%TRIPLET% ^
+.\vcpkg install ^
+    sdl2:%TRIPLET% ^
+    sdl2-mixer:%TRIPLET% ^
     yaml-cpp:%TRIPLET%
 
 REM Export libraries
-.\vcpkg export sdl2:%TRIPLET% ^
+.\vcpkg export ^
+    sdl2:%TRIPLET% ^
+    sdl2-mixer:%TRIPLET% ^
     yaml-cpp:%TRIPLET% ^
     --zip --nuget "--nuget-id=OpenLoco.Dependencies" "--nuget-version=%VERSION%"
 
