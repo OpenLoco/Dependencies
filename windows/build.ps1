f96532159f8a802339b56cb82e52568f2d238fa6
Margen67 <Margen67@users.noreply.github.com>
2022-02-17 04:59:07 -0800

    Build improvements (#7)
    ci.yml:
     Add workflow_dispatch: https://github.blog/changelog/2020-07-06-github-actions-manual-triggers-with-workflow_dispatch/
     Make VERSION env var global and add VCPKG_DISABLE_METRICS.
     Add matrix for other triplets (in the future)
     Opt out of PowerShell telemetry. (POWERSHELL_TELEMETRY_OPTOUT 1)
     Use checkout v2;
      fetch-depth: 0 is needed to keep behavior more consistent with v1.
     upload-artifact:
      Add VCPKG_DEFAULT_TRIPLET to name.
      Add if-no-files-found: error;
       Unlike v1, v2 doesn't error if it can't find a file by default.
    macos/build.sh:
     Make set into one command.
     Add -q to git clone and use pushd/popd for consistency with Windows.
     Use VPKG_DEFAULT_TRIPLET env var.
     Fix triplet copy.
    windows/*:
     Convert to PowerShell.
    windows/build:
     Remove TRIPLET:
      1. x86-windows is the default so it's unneeded.
      2. VCPKG_DEFAULT_TRIPLET does the same thing.
     Remove unneeded quotation marks.
    windows/install:
     Remove broken copy.

---
+++ windows/build.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference='Stop'
+
+pushd vcpkg
+
+$packages=@(
+    "benchmark";
+    "breakpad";
+    "gtest";
+    "libpng";
+    "sdl2";
+    "sdl2-mixer";
+    "yaml-cpp";
+)
+
+# Install libraries
+.\vcpkg install $packages
+
+# Export libraries
+.\vcpkg export $packages `
+    --zip --nuget --nuget-id=OpenLoco.Dependencies --nuget-version=$env:VERSION
+
+popd
