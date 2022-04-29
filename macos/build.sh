123bd527a6fa80fbd10d1ab29093c6adc9851041
Aaron van Geffen <aaron@aaronweb.net>
2022-04-29 20:21:25 +0200

    Drop SDL2_mixer from the dependency build scripts (#10)

--- macos/build.sh
+++ macos/build.sh
@@ -19,10 +19,9 @@ cp ../x86-osx.cmake triplets/
 	benchmark:x86-osx \
 	gtest:x86-osx \
 	libpng:x86-osx \
+	openal-soft:x86-osx \
 	sdl2:x86-osx \
-	sdl2-mixer:x86-osx \
-	yaml-cpp:x86-osx \
-	openal-soft:x86-osx
+	yaml-cpp:x86-osx
 
 # Zip the lot
 zip -r ../macos.dependencies.zip .vcpkg-root installed/ scripts/
